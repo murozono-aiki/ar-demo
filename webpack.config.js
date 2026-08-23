9a55c7ce8049a4eb16ce931905afc4871b8f828e
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2026-08-23 22:16:32 +0900

    Three.js のインストール

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export default {
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: {
+    "script": `./src/main.ts`
+  },
+
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${path.dirname(fileURLToPath(import.meta.url))}/docs`,
+    // 出力ファイル名
+    filename: "[name].js"
+  },
+
+  module: {
+    // node_modules由来のものを除くtsファイルに対してts-loaderを実行する
+    rules:[{
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+    }]
+  },
+
+  resolve: {
+    // import文で拡張子を省略可能にする
+    extensions: [".ts", ".js"],
+    // .js でのインポートを .ts に読み替えさせる
+    extensionAlias: {
+      '.js': ['.ts', '.js'],
+      '.mjs': ['.mts', '.mjs'] // mjsを使用している場合
+    }
+  },
+
+  mode: "production"
+};
