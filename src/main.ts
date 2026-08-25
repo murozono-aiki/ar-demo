1e2df596095be3449d79371e89e19274e772b993
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2026-08-25 12:53:20 +0900

    画像の上にボックスを追加

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,14 @@ const {renderer, scene, camera} = mindarThree;
 const anchor = mindarThree.addAnchor(0);
 const geometry = new THREE.PlaneGeometry(1, 0.55);
 const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
-const plane = new THREE.Mesh( geometry, material );
-anchor.group.add(plane.clone());
+const plane:THREE.Object3D = new THREE.Mesh( geometry, material );
+anchor.group.add(plane);
+
+const boxGeometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
+const box:THREE.Object3D = new THREE.Mesh(boxGeometry, material);
+anchor.group.add(box);
+box.translateZ(1)
+
 const start = async() => {
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
@@ -22,8 +28,8 @@ const startButton = document.getElementById("startButton") as HTMLButtonElement;
 startButton.addEventListener("click", () => {
     start();
 });
-const stopButton = document.getElementById("startButton") as HTMLButtonElement;
+const stopButton = document.getElementById("stopButton") as HTMLButtonElement;
 stopButton.addEventListener("click", () => {
     mindarThree.stop();
-    mindarThree.renderer.setAnimationLoop(null);
+    renderer.setAnimationLoop(null);
 });
