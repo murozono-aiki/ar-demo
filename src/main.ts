8ebf909c3ce163693376e148e7145f147deb8e47
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2026-09-01 23:26:06 +0900

    Three.jsの型定義をインストール・マーカーの変更

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import * as THREE from 'three';
 import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
 
+import markerMindUrl from './markers/ar-images.mind?url';
+
 const mindarThree = new MindARThree({
 	container: document.querySelector("#container"),
-	imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/card-example/card.mind",
-    maxTrack: undefined
+	imageTargetSrc: markerMindUrl,
+    maxTrack: 1
 });
 const {renderer, scene, camera} = mindarThree;
 const anchor = mindarThree.addAnchor(0);
 const geometry = new THREE.PlaneGeometry(1, 0.55);
-const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
-const plane:THREE.Object3D = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+const plane = new THREE.Mesh(geometry, material);
 anchor.group.add(plane);
 
 const boxGeometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
-const box:THREE.Object3D = new THREE.Mesh(boxGeometry, material);
+const box = new THREE.Mesh(boxGeometry, material);
 anchor.group.add(box);
 box.translateZ(1)
 
---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+declare module 'mind-ar/dist/mindar-image-three.prod.js' {
+  export const MindARThree: any;
+}
