70f568e39b8610112e43a968eadbf48d6b8b41a8
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2026-09-13 21:46:51 +0900

    4つ同時のトラッキングを実装

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import markerMindUrl from './markers/ar-images.mind?url';
 const mindarThree = new MindARThree({
 	container: document.querySelector("#container"),
 	imageTargetSrc: markerMindUrl,
-    maxTrack: 2
+    maxTrack: 4
 });
 const {renderer, scene, camera} = mindarThree;
 const anchor1 = mindarThree.addAnchor(0);
 const anchor2 = mindarThree.addAnchor(1);
+const anchor3 = mindarThree.addAnchor(2);
+const anchor4 = mindarThree.addAnchor(3);
 
 const geometry = new THREE.PlaneGeometry(1, 0.55);
 const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
@@ -26,6 +28,12 @@ const sphereGeometry = new THREE.SphereGeometry(0.4);
 const sphere = new THREE.Mesh(sphereGeometry, material);
 anchor2.group.add(sphere);
 
+const material2 = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.5});
+const sphere2 = new THREE.Mesh(sphereGeometry, material2);
+anchor3.group.add(sphere2);
+
+const box2 = new THREE.Mesh(boxGeometry, material2);
+anchor4.group.add(box2);
 
 const angularVelocity = 0.002;
 
@@ -34,6 +42,8 @@ const start = async() => {
     renderer.setAnimationLoop(time => {
         box.position.z = Math.cos(angularVelocity * time);
         sphere.position.z = -Math.cos(angularVelocity * time);
+        sphere2.position.z = Math.sin(angularVelocity * time);
+        box2.position.z = -Math.sin(angularVelocity * time);
         renderer.render(scene, camera);
     });
 }
